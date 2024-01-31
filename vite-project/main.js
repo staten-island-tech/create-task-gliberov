9f2e2c234169f6ffafb4115f7b2f9c8127cf7275
gliberov <124058342+gliberov@users.noreply.github.com>
2024-01-31 13:15:13 -0500

    yes

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,6 @@
 import './style.css'
+
+const api = 'https://opentdb.com/api.php?amount=50&type=boolean'
+const response = await fetch(api)
+const data = await response.json()
+console.log(data.results)
