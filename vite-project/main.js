2555367859a3deae783b5edb203de83e75401004
gliberov <124058342+gliberov@users.noreply.github.com>
2024-02-01 22:12:17 -0500

    yes

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,20 @@
-import './style.css'
-
 const api = 'https://opentdb.com/api.php?amount=50&type=boolean'
 const response = await fetch(api)
 const data = await response.json()
-console.log(data.results)
+const qa = data.results.map(question => 
+    ({question: question.question, 
+    correct: question.correct_answer, 
+    incorrect: question.incorrect_answers }))
+console.log(qa)
+const input = 5
+const used = []
+for (let i = 0; i < input; i++){
+    let q = qa[i]
+    if (q.correct.includes("True")){
+        console.log("BUSSSSSSSSSSSS")
+    }
+    if (q.correct.includes("False")){
+        console.log("OHHHHHHH")
+    }
+    
+}
